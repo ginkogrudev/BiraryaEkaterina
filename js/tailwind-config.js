94a4bd36610777897971511c17a8e9acd02d4402
ginkogrudev <ginko404@gmail.com>
2026-03-19 18:31:19 +0200

    Updated look is live

---
+++ js/tailwind-config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                outfit: ['Outfit', 'sans-serif'],
+            },
+            colors: {
+                ocean: {
+                    900: '#0a1a24',
+                    800: '#0f2e3c',
+                    card: '#164355',
+                },
+                surf: {
+                    accent: '#F5A623',
+                    pop: '#e85d04',
+                }
+            }
+        }
+    }
+}
